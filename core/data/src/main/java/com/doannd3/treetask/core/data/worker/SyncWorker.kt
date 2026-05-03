49fcd4a8ab7b90c27daf5493283289810d560aef
doannd3 <doannd3@ghtk.co>
2026-05-03 20:58:02 +0700

    setup log info app

--- core/data/src/main/java/com/doannd3/treetask/core/data/worker/SyncWorker.kt
+++ core/data/src/main/java/com/doannd3/treetask/core/data/worker/SyncWorker.kt
@@ -23,26 +23,26 @@ class SyncWorker
         private val userPrefsManager: UserPrefsManager,
     ) : CoroutineWorker(context, workerParams) {
         override suspend fun doWork(): Result {
-            Timber.tag(AppTag.DATA).i("Bắt đầu quá trình đồng bộ hóa dữ liệu ngầm...")
+            Timber.tag(AppTag.NETWORK).i("Bắt đầu quá trình đồng bộ hóa dữ liệu ngầm...")
 
             // 1. Lấy thông tin User hiện tại (hứng phần tử đầu tiên của Flow)
             val user = userPrefsManager.getUserProfile().firstOrNull()
 
             // Nếu user đã đăng xuất, hủy luôn nhiệm vụ này (failure), không cần làm gì cả
             if (user == null || user.id.isBlank()) {
-                Timber.tag(AppTag.DATA).w("Không có User đăng nhập. Hủy đồng bộ.")
+                Timber.tag(AppTag.NETWORK).w("Không có User đăng nhập. Hủy đồng bộ.")
                 return Result.failure()
             }
 
             // 2. Kêu gọi TaskRepository đồng bộ dữ liệu của ông user này
             return when (val result = taskRepository.syncTasks(user.id)) {
                 is ApiResult.Success -> {
-                    Timber.tag(AppTag.DATA).i("Đồng bộ THÀNH CÔNG!")
+                    Timber.tag(AppTag.NETWORK).i("Đồng bộ THÀNH CÔNG!")
                     Result.success() // Báo cáo sếp: Đã xong việc!
                 }
 
                 is ApiResult.Error -> {
-                    Timber.tag(AppTag.DATA).e(result.exception, "Đồng bộ THẤT BẠI.")
+                    Timber.tag(AppTag.NETWORK).e(result.exception, "Đồng bộ THẤT BẠI.")
                     // Báo cáo sếp: Xin hãy gọi em dậy chạy lại nhiệm vụ này khi mạng ổn định hơn!
                     Result.retry()
                 }
