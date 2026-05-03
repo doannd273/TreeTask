be4171c1eb59ebeda84ce71cbc0cd477ebcad13b
doannd3 <doannd3@ghtk.co>
2026-05-03 16:31:56 +0700

    core[analytics]: setup firebase analytics, performance helper

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     // google, firebase
     alias(libs.plugins.google.services)
     alias(libs.plugins.firebase.crashlytics)
+    alias(libs.plugins.firebase.perf)
     // lint
     alias(libs.plugins.treetask.android.detekt)
     alias(libs.plugins.treetask.android.spotless)
@@ -80,7 +81,7 @@ dependencies {
     implementation(projects.feature.stats)
     implementation(projects.feature.tasks)
 
-// core
+    // core
     implementation(projects.core.domain)
     implementation(projects.core.common)
     implementation(projects.core.designsystem)
@@ -96,7 +97,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    testImplementation(libs.junit)
+
+    // testing
+    testImplementation(projects.core.testing)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
@@ -124,4 +127,5 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.crashlytics)
+    implementation(libs.firebase.perf)
 }
--- core/analytics/src/main/java/com/doannd3/treetask/core/analytics/FirebaseAnalyticsHelper.kt
+++ core/analytics/src/main/java/com/doannd3/treetask/core/analytics/FirebaseAnalyticsHelper.kt
@@ -1,20 +1,37 @@
 package com.doannd3.treetask.core.analytics
 
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.logEvent
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class FirebaseAnalyticsHelper
-@Inject
-constructor() : AnalyticsHelper {
-    override fun setUserId(userId: String) {
-        val crashlytics = FirebaseCrashlytics.getInstance()
-        crashlytics.setUserId(userId)
-    }
+    @Inject
+    constructor(
+        private val firebaseCrashlytics: FirebaseCrashlytics,
+        private val firebaseAnalytics: FirebaseAnalytics,
+    ) : AnalyticsHelper {
+        override fun setUserId(userId: String) {
+            firebaseCrashlytics.setUserId(userId)
+        }
 
-    override fun clearUserId() {
-        val crashlytics = FirebaseCrashlytics.getInstance()
-        crashlytics.setUserId("")
+        override fun clearUserId() {
+            firebaseCrashlytics.setUserId("")
+        }
+
+        override fun logEvent(event: AnalyticsEvent) {
+            firebaseAnalytics.logEvent(event.type) {
+                for (param in event.extras) {
+                    // Firebase quy định value tối đa 100 ký tự để tối ưu băng thông
+                    param(param.key, param.value.take(MAX_PARAM_LENGTH))
+                }
+            }
+        }
+
+        // Khai báo hằng số ở đây
+        companion object {
+            private const val MAX_PARAM_LENGTH = 100
+        }
     }
-}
