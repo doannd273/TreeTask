d3dec4ab1a227533c1e82ecf7eda756f0bef489c
doannd3 <doannd3@ghtk.co>
2026-05-03 16:35:22 +0700

    core[analytics]: format lint

--- app/src/main/java/com/treestudio/treetask/initializer/TimberInitializer.kt
+++ app/src/main/java/com/treestudio/treetask/initializer/TimberInitializer.kt
@@ -6,7 +6,7 @@ import timber.log.Timber
 import javax.inject.Inject
 
 class TimberInitializer @Inject constructor(
-    private val crashlyticsTree: CrashlyticsTree
+    private val crashlyticsTree: CrashlyticsTree,
 ) : AppInitializer {
 
     // Timber phải chạy cực sớm!
