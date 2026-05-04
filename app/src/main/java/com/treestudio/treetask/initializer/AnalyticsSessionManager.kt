70fc24788084a9c8c07b98303ae96534e2ce4d4c
doannd3 <doannd3@ghtk.co>
2026-05-04 17:05:58 +0700

    log: update

--- app/src/main/java/com/treestudio/treetask/initializer/AnalyticsSessionManager.kt
+++ app/src/main/java/com/treestudio/treetask/initializer/AnalyticsSessionManager.kt
@@ -8,25 +8,33 @@ import com.doannd3.treetask.core.domain.repository.UserRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class AnalyticsSessionManager @Inject constructor(
-    private val userRepository: UserRepository,
-    private val analyticsHelper: AnalyticsHelper,
-    @Dispatcher(TreeTaskDispatchers.IO) private val ioDispatcher: CoroutineDispatcher,
-) : AppInitializer {
-    private val applicationScope = CoroutineScope(SupervisorJob() + ioDispatcher)
+class AnalyticsSessionManager
+    @Inject
+    constructor(
+        private val userRepository: UserRepository,
+        private val analyticsHelper: AnalyticsHelper,
+        @Dispatcher(TreeTaskDispatchers.IO) private val ioDispatcher: CoroutineDispatcher,
+    ) : AppInitializer {
+        private val applicationScope = CoroutineScope(SupervisorJob() + ioDispatcher)
 
-    override fun init(application: Application) {
-        applicationScope.launch {
-            userRepository.getCachedProfile().collect { user ->
-                if (user != null) {
-                    analyticsHelper.setUserId(user.id)
-                } else {
-                    analyticsHelper.clearUserId()
-                }
+        override fun init(application: Application) {
+            applicationScope.launch {
+                userRepository
+                    .getCachedProfile()
+                    .map { user -> user?.id }
+                    .distinctUntilChanged()
+                    .collect { userId ->
+                        if (userId != null) {
+                            analyticsHelper.setUserId(userId)
+                        } else {
+                            analyticsHelper.clearUserId()
+                        }
+                    }
             }
         }
     }
-}
--- core/analytics/src/main/java/com/doannd3/treetask/core/analytics/FirebaseAnalyticsHelper.kt
+++ core/analytics/src/main/java/com/doannd3/treetask/core/analytics/FirebaseAnalyticsHelper.kt
@@ -8,30 +8,32 @@ import javax.inject.Singleton
 
 @Singleton
 class FirebaseAnalyticsHelper
-@Inject
-constructor(
-    private val firebaseCrashlytics: FirebaseCrashlytics,
-    private val firebaseAnalytics: FirebaseAnalytics,
-) : AnalyticsHelper {
-    override fun setUserId(userId: String) {
-        firebaseCrashlytics.setUserId(userId)
-    }
+    @Inject
+    constructor(
+        private val firebaseCrashlytics: FirebaseCrashlytics,
+        private val firebaseAnalytics: FirebaseAnalytics,
+    ) : AnalyticsHelper {
+        override fun setUserId(userId: String) {
+            firebaseCrashlytics.setUserId(userId)
+            firebaseAnalytics.setUserId(userId)
+        }
 
-    override fun clearUserId() {
-        firebaseCrashlytics.setUserId("")
-    }
+        override fun clearUserId() {
+            firebaseCrashlytics.setUserId("")
+            firebaseAnalytics.setUserId(null)
+        }
 
-    override fun logEvent(event: AnalyticsEvent) {
-        firebaseAnalytics.logEvent(event.type) {
-            for (param in event.extras) {
-                // Firebase quy định value tối đa 100 ký tự để tối ưu băng thông
-                param(param.key, param.value.take(MAX_PARAM_LENGTH))
+        override fun logEvent(event: AnalyticsEvent) {
+            firebaseAnalytics.logEvent(event.type) {
+                for (param in event.extras) {
+                    // Firebase quy định value tối đa 100 ký tự để tối ưu băng thông
+                    param(param.key, param.value.take(MAX_PARAM_LENGTH))
+                }
             }
         }
-    }
 
-    // Khai báo hằng số ở đây
-    companion object {
-        private const val MAX_PARAM_LENGTH = 100
+        // Khai báo hằng số ở đây
+        companion object {
+            private const val MAX_PARAM_LENGTH = 100
+        }
     }
-}
