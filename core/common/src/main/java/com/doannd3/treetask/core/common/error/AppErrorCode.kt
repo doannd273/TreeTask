4d6a39b4fca1fdc09f48521d9c3f3e608d540053
doannd3 <doannd3@ghtk.co>
2026-05-25 16:05:18 +0700

    TRE-25: feat(profile): add avatar upload use case

--- core/common/src/main/java/com/doannd3/treetask/core/common/error/AppErrorCode.kt
+++ core/common/src/main/java/com/doannd3/treetask/core/common/error/AppErrorCode.kt
@@ -5,6 +5,7 @@ import com.doannd3.treetask.core.common.UiText
 
 enum class AppErrorCode {
     MISSING_RESPONSE_DATA,
+    UNSUPPORTED_MEDIA_TYPE,
     UNKNOWN,
 }
 
@@ -12,6 +13,9 @@ fun AppErrorCode.toUiText(): UiText =
     when (this) {
         AppErrorCode.MISSING_RESPONSE_DATA ->
             UiText.StringResource(R.string.common_missing_response_data)
+
+        AppErrorCode.UNSUPPORTED_MEDIA_TYPE ->
+            UiText.StringResource(R.string.common_error_unsupported_media_type)
 
         AppErrorCode.UNKNOWN ->
             UiText.StringResource(R.string.common_error_unknown)
