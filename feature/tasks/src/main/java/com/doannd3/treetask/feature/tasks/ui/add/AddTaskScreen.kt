f1450d9db1b0729b864e452df353675802fc4025
doannd3 <doannd3@ghtk.co>
2026-05-27 13:21:15 +0700

    TRE-52: feat(tasks): define AddTaskContract and implement AddTaskViewModel

--- feature/tasks/src/main/java/com/doannd3/treetask/feature/tasks/ui/add/AddTaskScreen.kt
+++ feature/tasks/src/main/java/com/doannd3/treetask/feature/tasks/ui/add/AddTaskScreen.kt
@@ -1,3 +1,69 @@
 package com.doannd3.treetask.feature.tasks.ui.add
 
-class AddTaskScreen
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.repeatOnLifecycle
+import com.doannd3.treetask.core.common.asString
+import com.doannd3.treetask.core.designsystem.component.LocalGlobalAppState
+
+@Composable
+fun AddTaskRoute(
+    viewModel: AddTaskViewModel = hiltViewModel(),
+    onNavigateBack: () -> Unit,
+) {
+    val state by viewModel.uiState.collectAsStateWithLifecycle()
+
+    val globalAppState = LocalGlobalAppState.current
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+//    AddTaskScreen(
+//        state = state,
+//        onEvent = viewModel::onEvent,
+//        onNavigateBack = onNavigateBack,
+//    )
+
+    LaunchedEffect(viewModel.effect, lifecycleOwner) {
+        lifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            viewModel.effect.collect { effect ->
+                when (effect) {
+                    is AddTaskEffect.ShowErrorMessage -> {
+                        globalAppState.showError(effect.message.asString(context))
+                    }
+
+                    is AddTaskEffect.ShowSuccessMessage -> {
+                        globalAppState.showSuccess(effect.message.asString(context)) {
+                            viewModel.onEvent(AddTaskEvent.SuccessAcknowledged)
+                        }
+                    }
+
+                    is AddTaskEffect.NavigateBack -> {
+                        onNavigateBack()
+                    }
+                }
+            }
+        }
+    }
+
+    LaunchedEffect(viewModel.baseErrorEffect, lifecycleOwner) {
+        lifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            viewModel.baseErrorEffect.collect { message ->
+                globalAppState.showError(message.asString(context))
+            }
+        }
+    }
+
+    LaunchedEffect(state.isLoading) {
+        if (state.isLoading) {
+            globalAppState.showLoading()
+        } else {
+            globalAppState.hideLoading()
+        }
+    }
+}
