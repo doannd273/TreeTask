63992217c1eef11fa0acd769f8a8d65316330598
doannd3 <doannd3@ghtk.co>
2026-05-27 13:21:15 +0700

    TRE-52: feat(tasks): define AddTaskContract and implement AddTaskViewModel

--- core/model/src/main/java/com/doannd3/treetask/core/model/task/TaskStatus.kt
+++ core/model/src/main/java/com/doannd3/treetask/core/model/task/TaskStatus.kt
@@ -7,7 +7,8 @@ enum class TaskStatus(
     TODO("todo", "Cần làm"),
     IN_PROGRESS("in_progress", "Đang thực hiện"),
     PENDING("pending", "Đang chờ"),
-    DONE("done", "Hoàn thành"), ;
+    DONE("done", "Hoàn thành"),
+    ;
 
     companion object {
         fun fromStatus(status: String?): TaskStatus =
