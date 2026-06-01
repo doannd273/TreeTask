7ef4f08682f71a9e0fb7a125d4a29dc064f5b48c
doannd3 <doannd3@ghtk.co>
2026-06-01 11:16:30 +0700

    TRE-90: feat(auth): unregister FCM device token on logout

--- core/domain/src/main/java/com/doannd3/treetask/core/domain/repository/DeviceRepository.kt
+++ core/domain/src/main/java/com/doannd3/treetask/core/domain/repository/DeviceRepository.kt
@@ -4,4 +4,6 @@ import com.doannd3.treetask.core.common.ApiResult
 
 interface DeviceRepository {
     suspend fun registerToken(token: String): ApiResult<Unit>
+
+    suspend fun unregisterToken(token: String): ApiResult<Unit>
 }
--- core/network/src/main/java/com/doannd3/treetask/core/network/service/DeviceService.kt
+++ core/network/src/main/java/com/doannd3/treetask/core/network/service/DeviceService.kt
@@ -2,12 +2,19 @@ package com.doannd3.treetask.core.network.service
 
 import com.doannd3.treetask.core.common.ApiResult
 import com.doannd3.treetask.core.network.model.request.RegisterDeviceTokenRequest
+import com.doannd3.treetask.core.network.model.request.UnRegisterTokenRequest
 import retrofit2.http.Body
+import retrofit2.http.HTTP
 import retrofit2.http.POST
 
 interface DeviceService {
     @POST("/api/devices/register-token")
     suspend fun registerToken(
         @Body body: RegisterDeviceTokenRequest,
+    ): ApiResult<Unit>
+
+    @HTTP(method = "DELETE", path = "/api/devices/unregister-token", hasBody = true)
+    suspend fun unregisterToken(
+        @Body body: UnRegisterTokenRequest,
     ): ApiResult<Unit>
 }
