a0da4315b62ee89479af5c00144936b8b384b528
doannd3 <doannd3@ghtk.co>
2026-06-01 11:16:30 +0700

    TRE-90: feat(auth): unregister FCM device token on logout

--- core/domain/src/main/java/com/doannd3/treetask/core/domain/usecase/device/UnRegisterDeviceTokenUseCase.kt
+++ core/domain/src/main/java/com/doannd3/treetask/core/domain/usecase/device/UnRegisterDeviceTokenUseCase.kt
@@ -1,4 +1,15 @@
 package com.doannd3.treetask.core.domain.usecase.device
 
-class UnRegisterDeviceTokenUseCase {
-}
+import com.doannd3.treetask.core.common.ApiResult
+import com.doannd3.treetask.core.domain.repository.DeviceRepository
+import javax.inject.Inject
+
+class UnRegisterDeviceTokenUseCase
+    @Inject
+    constructor(
+        private val deviceRepository: DeviceRepository,
+    ) {
+        suspend operator fun invoke(token: String): ApiResult<Unit> {
+            return deviceRepository.unregisterToken(token = token)
+        }
+    }
