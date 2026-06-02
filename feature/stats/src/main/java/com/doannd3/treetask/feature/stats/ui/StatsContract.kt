bad6f985c01e35369c671faf1fd348b935235806
doannd3 <doannd3@ghtk.co>
2026-06-02 11:47:51 +0700

    TRE-107: feat(stats): wire viewmodel to task stats use case

--- feature/stats/src/main/java/com/doannd3/treetask/feature/stats/ui/StatsContract.kt
+++ feature/stats/src/main/java/com/doannd3/treetask/feature/stats/ui/StatsContract.kt
@@ -1,9 +1,23 @@
 package com.doannd3.treetask.feature.stats.ui
 
+import com.doannd3.treetask.core.common.UiText
+import com.doannd3.treetask.core.model.stats.TaskStats
+
 data class StatsState(
     val isLoading: Boolean = false,
-)
+    val isRefreshing: Boolean = false,
+    val taskStats: TaskStats? = null,
+    val hasInitialLoadError: Boolean = false,
+) {
+    val isEmpty: Boolean get() = taskStats?.total == 0
+}
 
-sealed class StatsEvent
+sealed class StatsEvent {
+    data object Refresh : StatsEvent()
+}
 
-sealed class StatsEffect
+sealed class StatsEffect {
+    data class ShowErrorMessage(
+        val message: UiText,
+    ) : StatsEffect()
+}
